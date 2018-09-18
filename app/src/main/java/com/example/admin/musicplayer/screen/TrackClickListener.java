de584a726ec4a3b29d440c33aa0631fd2a951098
thantam03011997@gmail.com <thantam03011997@gmail.com>
2018-09-18 17:47:57 +0700

    handle logic home screen

---
+++ app/src/main/java/com/example/admin/musicplayer/screen/TrackClickListener.java
@@ -0,0 +1,11 @@
+package com.example.admin.musicplayer.screen;
+
+import com.example.admin.musicplayer.data.model.Track;
+
+/**
+ * Created by TamTT on 9/16/2018.
+ */
+
+public interface TrackClickListener {
+    void onTrackClicked(Track track);
+}
---
+++ app/src/main/java/com/example/admin/musicplayer/screen/home/HomeFragment.java
@@ -0,0 +1,62 @@
+package com.example.admin.musicplayer.screen.home;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.admin.musicplayer.R;
+import com.example.admin.musicplayer.data.model.Genre;
+import com.example.admin.musicplayer.databinding.FragmentHomeBinding;
+import com.example.admin.musicplayer.screen.HandlerClick;
+
+import java.util.List;
+
+/**
+ * Created by TamTT on 9/15/2018.
+ */
+
+public class HomeFragment extends Fragment{
+
+    private HomeViewModel mHomeViewModel;
+    private FragmentHomeBinding mBinding;
+    private GenreAdapter mAdapter;
+    private HandlerClick mHandlerClick;
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHomeViewModel = ViewModelProviders.of(getActivity()).
+                get(HomeViewModel.class);
+        mHandlerClick = new HandlerClick(getActivity());
+        mAdapter = new GenreAdapter(getContext(), mHandlerClick);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_home, container, false);
+        mBinding.recyclerGenres.setAdapter(mAdapter);
+        View view = mBinding.getRoot();
+        mHomeViewModel.getAllGenres().observe(this, new Observer<List<Genre>>() {
+            @Override
+            public void onChanged(@Nullable List<Genre> genres) {
+                mAdapter.setGenreList(genres);
+            }
+        });
+        return view;
+    }
+}
